eb33ef53c4d40ce230db00d083f66a99a032a5f9
Tony Ho <ho.tonym@gmail.com>
2018-05-13 05:00:17 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,16 +1,16 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season].each do |array_of_contestant|
-    if array_of_contestant["status"] == "Winner"
-      return array_of_contestant["name"].split(" ").first
+  data[season].each do |array|
+    if array["status"] == "Winner"
+       array["name"].split(" ").first
     end
   end
 end
 
  def get_contestant_name(data, occupation)
   # code here
-  data.each do |season, array_of_contestant|
-    array_of_contestant.each do |contestants|
+  data.each do |season, array_of_contestants|
+    array_of_contestants.each do |contestants|
       if contestants["occupation"] == occupation
         return contestants["name"]
       end
@@ -21,8 +21,8 @@ end
  def count_contestants_by_hometown(data, hometown)
   # code here
  counter = 0
- data.each do |season, array_of_contestant|
-   array_of_contestant.each do |contestants|
+ data.each do |season, array_of_contestants|
+   array_of_contestants.each do |contestants|
      if contestants["hometown"] == hometown
        counter += 1
      end
@@ -33,8 +33,8 @@ end
 
 def get_occupation(data, hometown)
   # code here
-  data.each do |season, array_of_contestant|
-    array_of_contestant.each do |contestants|
+  data.each do |season, array_of_contestants|
+    array_of_contestants.each do |contestants|
       if contestants["hometown"] == hometown
         return contestants["occupation"]
       end
@@ -46,8 +46,8 @@ end
   # code here
   ages = []
   ages_as_int= []
-  data[season].each do |array_of_contestant|
-    ages << array_of_contestant["age"]
+  data[season].each do |array_of_contestants|
+    ages << array_of_contestants["age"]
   end
   ages.collect do |x|
     ages_as_int << x.to_f
