897b3d9b49338b3d72a47c03e8d3fe55ad55612c
Tony Ho <ho.tonym@gmail.com>
2018-05-13 05:05:44 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,16 +1,16 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season].each do |array_of_contestant|
-    if array_of_contestant["status"] == "Winner"
-      return array_of_contestant["name"].split(" ").first
+  data[season].each do |element|
+    if element["status"] == "Winner"
+      return element["name"].split(" ").first
     end
   end
 end
 
  def get_contestant_name(data, occupation)
   # code here
-  data.each do |season, array_of_contestant|
-    array_of_contestant.each do |contestants|
+  data.each do |season, element|
+    element.each do |contestants|
       if contestants["occupation"] == occupation
         return contestants["name"]
       end
@@ -21,8 +21,8 @@ end
  def count_contestants_by_hometown(data, hometown)
   # code here
  counter = 0
- data.each do |season, array_of_contestant|
-   array_of_contestant.each do |contestants|
+ data.each do |season, element|
+   element.each do |contestants|
      if contestants["hometown"] == hometown
        counter += 1
      end
@@ -33,8 +33,8 @@ end
 
 def get_occupation(data, hometown)
   # code here
-  data.each do |season, array_of_contestant|
-    array_of_contestant.each do |contestants|
+  data.each do |season, element|
+    element.each do |contestants|
       if contestants["hometown"] == hometown
         return contestants["occupation"]
       end
@@ -44,13 +44,14 @@ end
 
  def get_average_age_for_season(data, season)
   # code here
-  ages = []
-  ages_as_int= []
-  data[season].each do |array_of_contestant|
-    ages << array_of_contestant["age"]
+
+  ages = data[season].map do |element|
+      element["age"]
   end
-  ages.collect do |x|
-    ages_as_int << x.to_f
+
+  ages_int = ages.map do |number|
+    number.to_f
   end
-  (ages_as_int.reduce(:+) / ages.length).round(0)
+
+  (ages_int.reduce(:+) / ages.length).round(0)
 end
